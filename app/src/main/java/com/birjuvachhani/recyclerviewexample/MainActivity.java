f59f2da75e7d6f398f8ee0e415dfa330c9da7897
birju.vachhani <brvachhani@gmail.com>
2018-01-19 18:31:15 +0530

    Add xml of single row for the List

--- app/src/main/java/com/birjuvachhani/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/birjuvachhani/recyclerviewexample/MainActivity.java
@@ -2,12 +2,17 @@ package com.birjuvachhani.recyclerviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mrecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mrecyclerView=(RecyclerView)findViewById(R.id.recycler_view);
     }
 }
