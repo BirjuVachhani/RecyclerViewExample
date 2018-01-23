0aab5d053d886da186017641afdbea37bda30101
birju.vachhani <brvachhani@gmail.com>
2018-01-23 15:24:07 +0530

    Add data binding to Detailed Activity

--- app/src/main/java/com/birjuvachhani/recyclerviewexample/DetailedActivity.java
+++ app/src/main/java/com/birjuvachhani/recyclerviewexample/DetailedActivity.java
@@ -1,35 +1,30 @@
 package com.birjuvachhani.recyclerviewexample;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.birjuvachhani.recyclerviewexample.databinding.DetailedActivityBinding;
+
 public class DetailedActivity extends AppCompatActivity {
 
-    private ImageView mimg;
-    private TextView mtitle;
-    private TextView mdesc;
+    private DetailedActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed);
 
-        mimg=(ImageView)findViewById(R.id.iv_img);
-        mtitle=(TextView)findViewById(R.id.tv_title);
-        mdesc=(TextView)findViewById(R.id.tv_desc);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_detailed);
 
         if(getIntent()!=null)
         {
             Intent intent=getIntent();
-
             DataHolder data=intent.getParcelableExtra(Constants.PARCEL_LABEL);
-
-            mimg.setImageResource(data.getImageId());
-            mtitle.setText(data.getTitle());
-            mdesc.setText(data.getDesc());
+            binding.setDataholder(data);
+            binding.executePendingBindings();
         }
     }
 }
